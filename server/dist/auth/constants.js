a51a9e7c43dc0d994ea893d84a5f292a36bd5e71
Prashant Gunjal <prashantgunjal.dev@gmail.com>
2022-02-01 10:28:20 +0530

    Added JWT token based validation and verification

---
+++ server/dist/auth/constants.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.jwtConstants = void 0;
+exports.jwtConstants = {
+    secret: 'asgah@#11333gdhsbjm',
+};
+//# sourceMappingURL=constants.js.map
---
+++ server/src/auth/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+    secret: 'asgah@#11333gdhsbjm',
+  };
